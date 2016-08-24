e19ef13e746c339550cffff2a005fb719bb51078
nguyenminhtiend <nguyenminhtiend@gmail.com>
2016-08-24 22:53:02 +0700

    #init
    #init

---
+++ src/src/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace src
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
